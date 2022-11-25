11fe7f9acdff0f540b084842fc483b9bf17066d8
gejeduck <47668701+gejeduck@users.noreply.github.com>
2022-11-25 21:33:12 +1000

    fix: mainnet config

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,45 +1,103 @@
-import { ExecuteEvent, Message } from "../types";
-import {
-  CosmosEvent,
-  CosmosBlock,
-  CosmosMessage,
-  CosmosTransaction,
-} from "@subql/types-cosmos";
+import { Request, Account } from "../types";
+import { TerraEvent } from "@subql/types-terra";
+import { MsgExecuteContract } from "@terra-money/terra.js";
 
-/*
-export async function handleBlock(block: CosmosBlock): Promise<void> {
-  // If you want to index each block in Cosmos (Juno), you could do that here
+const checkRequest = (
+  event: TerraEvent<MsgExecuteContract>,
+  action: string
+) => {
+  if (
+    event.event.attributes[0].value ==
+      "terra1zp04qw3v3vehgnrczmgd3t3x6tcktprcwyjhgk3xvsj7mh2xdkdqr9d8uz" &&
+    event.event.attributes[1].value == action
+  ) {
+    return true;
+  }
+  return false;
+};
+
+export async function handleCreateRequestEvent(
+  event: TerraEvent<MsgExecuteContract>
+): Promise<void> {
+  if (!checkRequest(event, "create_request")) {
+    return;
+  }
+
+  const accountId = event.event.attributes[3].value;
+  const target = event.event.attributes[4].value;
+  const msg = event.event.attributes[5].value;
+  const asset = event.event.attributes[6].value;
+
+  const requestId = event.event.attributes[2].value;
+  logger.info(`create_request: id = ${requestId}`);
+
+  let account = await Account.get(accountId);
+  if (account === undefined) {
+    account = Account.create({
+      id: accountId,
+      address: accountId,
+    });
+    await account.save();
+  }
+
+  const request = Request.create({
+    id: requestId,
+    registerId: accountId,
+    txHash: event.tx.tx.txhash,
+    target,
+    msg,
+    assets: asset.slice(11, -1),
+    executor: "",
+    status: "created",
+    createdAt: event.block.block.block.header.time,
+    executedOrCancelledAt: "",
+  });
+
+  await request.save();
 }
 
-export async function handleTransaction(tx: CosmosTransaction): Promise<void> {
-  // If you want to index each transaction in Cosmos (Juno), you could do that here
-  const transactionRecord = Transaction.create({
-    id: tx.hash,
-    blockHeight: BigInt(tx.block.block.header.height),
-    timestamp: tx.block.block.header.time,
-  });
-  await transactionRecord.save();
+export async function handleExecuteRequestEvent(
+  event: TerraEvent<MsgExecuteContract>
+): Promise<void> {
+  if (!checkRequest(event, "execute_request")) {
+    return;
+  }
+
+  const requestId = event.event.attributes[2].value;
+  logger.info(`execute_request: id = ${requestId}`);
+
+  const request = await Request.get(requestId);
+  if (request === undefined) {
+    logger.error(`execute_request: not found: id = ${requestId}`);
+    return;
+  }
+
+  request.executor =
+    event.msg.tx.tx.tx.auth_info.signer_infos[0].public_key.address();
+  request.status = "executed";
+  request.executedOrCancelledAt = event.block.block.block.header.time;
+
+  await request.save();
 }
-*/
 
-export async function handleMessage(msg: CosmosMessage): Promise<void> {
-  const messageRecord = Message.create({
-    id: `${msg.tx.hash}-${msg.idx}`,
-    blockHeight: BigInt(msg.block.block.header.height),
-    txHash: msg.tx.hash,
-    sender: msg.msg.decodedMsg.sender,
-    contract: msg.msg.decodedMsg.contract,
-  });
-  await messageRecord.save();
-}
+export async function handleCancelRequestEvent(
+  event: TerraEvent<MsgExecuteContract>
+): Promise<void> {
+  if (!checkRequest(event, "cancel_request")) {
+    return;
+  }
 
-export async function handleEvent(event: CosmosEvent): Promise<void> {
-  const eventRecord = ExecuteEvent.create({
-    id: `${event.tx.hash}-${event.msg.idx}-${event.idx}`,
-    blockHeight: BigInt(event.block.block.header.height),
-    txHash: event.tx.hash,
-    contractAddress: event.event.attributes.find(attr => attr.key === '_contract_address').value
-  });
+  const requestId = event.event.attributes[2].value;
+  logger.info(`cancel_request: id = ${requestId}`);
 
-  await eventRecord.save();
+  const request = await Request.get(requestId);
+  if (request === undefined) {
+    logger.error(`cancel_request: not found: id = ${requestId}`);
+    return;
+  }
+
+  request.status = "cancelled";
+  request.executedOrCancelledAt = event.block.block.block.header.time;
+
+  await request.save();
 }
